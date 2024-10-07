b34d41927ec593b4f6c49be29d906f3987653b7d
Omar Mousa <omar.s.mousa194@gmail.com>
2024-10-07 09:28:09 +0300

    add export to auth and db

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -12,6 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
