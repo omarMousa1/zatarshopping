5fc4faea7117492e6918a1f839a44813b626cc9c
Omar Mousa <omar.s.mousa194@gmail.com>
2024-10-07 09:53:50 +0300

    create useRegister

---
+++ src/hooks/useRegister.ts
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { auth, db } from "../firebase/firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
+export const useRegister = () => {
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<number>();
+  const [error, setError] = useState<string>("");
+  const [load, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const register = async () => {
+    setError("");
+    setLoading(true);
+
+    if (password !== confirmPassword) {
+      setError("Password do not match");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      const userData = {
+        username,
+        email,
+        phoneNumber,
+        uid: user.uid,
+        createdAt: new Date(),
+      };
+
+      await setDoc(doc(db, "users", user.uid), userData);
+      console.log("User registered successfully:", userData);
+      navigate(""); //Dashboard
+    } catch (err: any) {
+      setError(err.message);
+      console.log("Error registering user:", err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    username,
+    setUsername,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    confirmPassword,
+    setConfirmPassword,
+    phoneNumber,
+    setPhoneNumber,
+    error,
+    load,
+    register,
+  };
+};
