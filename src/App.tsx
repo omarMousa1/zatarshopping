e2650c97a8cfb2b13de0f167aea01494ea5c5059
Omar Mousa <omar.s.mousa194@gmail.com>
2024-10-08 07:56:44 +0300

    create home and dashboard, add router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import { auth } from "./firebase/firebaseConfig";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Home } from "./pages/Home"
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
+
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, [])
+
   return (
-    <div>
-      <p>ola</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Dashboard /> : <Home />} />
+        <Route path="/register" element={user ? <Navigate to="/dashboard" /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Home />} />
+      </Routes>
+    </Router>
   );
 }
 
