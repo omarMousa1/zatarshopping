5cccb47ca70089e4e054ba086e80446fbcf0111e
Omar Mousa <omar.s.mousa194@gmail.com>
2024-10-15 13:20:40 +0300

    create useFakeData, product.ts

---
+++ src/hooks/useFakeData.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { Product } from "../types/product";
+
+export const useFakeData = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response) {
+          throw new Error("Network response was not ok");
+        }
+        const data: Product[] = await response.json();
+        setProducts(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("An unknown error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+};
