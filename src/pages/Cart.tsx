b541224271eafce0c059f53c0ba6cb9690935292
Omar Mousa <omar.s.mousa194@gmail.com>
2024-10-27 12:46:59 +0300

    edit style in cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,6 +3,7 @@ import { store } from '../hooks/store'
 import { NavLink } from 'react-router-dom';
 import { useCalculateTotal } from '../hooks/useCalculateTotal';
 import { useUpdateQuantity } from '../hooks/useUpdateQuantity';
+import { Product } from '../types/product';
 
 
 export const Cart = () => {
@@ -18,32 +19,40 @@ export const Cart = () => {
 
 
   return (
-    <div>
-        <h1 className='text-3xl font-semibold mb-6 text-center'>Your cart</h1>
-        <div>
-            {cart.map((item) => (
-                <div key={item.id} className='flex justify-between items-center border-b pb-4'>
-                    <div>
-                        <p>{item.title}</p>
-                        <p>x {item.quantity}</p>
-                        <div className='flex space-x-2 mt-2'>
-                            <button onClick={() => increaseQuantity(item.id)} className='bg-green-500 text-white px-4 rounded-md'>+</button>
-                            <button onClick={() => decreaseQuantity(item.id)} className='bg-red-500 text-white px-4 rounded-md'>-</button>
+    <div className=''>
+        <h1 className='text-3xl font-semibold mb-6 text-center'>Shopping cart</h1>
+        <div className='flex flex-col justify-center items-center rounded-lg shadow-2xl shadow-black m-4 p-5'>
+            <div className='space-y-4 w-full'>
+                <div className='grid grid-cols-3 place-items-center mb-12'>
+                    <p className='font-bold text-2xl italic'>Product</p>                 
+                    <p className='font-bold text-2xl italic'>Price</p> 
+                    <p className='font-bold text-2xl italic'>Quantity</p> 
+                </div>
+                {cart.map((item) => (
+                    <>
+                    <div key={item.id} className='grid grid-cols-3 text-center'>
+                        <p className='text-left'>{item.title}</p>
+                        <p className=''>${(item.price).toFixed(2)}</p>
+                        <div className='flex justify-center items-center space-x-3'>
+                            <button onClick={() => decreaseQuantity(item.id)} className='flex justify-center items-center border border-gray-400 text-gray-400 px-4 rounded-md w-8 h-8'>-</button>
+                            <p>{item.quantity}</p>
+                            <button onClick={() => increaseQuantity(item.id)} className='flex justify-center items-center border border-gray-400 text-gray-400 px-4 rounded-md w-8 h-8'>+</button>
                         </div>
                     </div>
-                    <p>${(item.price).toFixed(2)}</p>
-                </div>
-            ))}
-        </div>
-        
-        <div className='mt-6 flex justify-between text-xl font-semibold'>
-            <span>Total:</span>
-            <span>${calTotal()}</span>
-        </div>
+                    <hr />
+                    </>
+                ))}
+            </div>
+            
+            <div className='flex justify-center items-center gap-x-5 my-6 p-2 rounded-lg shadow-lg text-xl font-semibold border border-gray-500 w-max'>
+                <span>Total:</span>
+                <span>${calTotal()}</span>
+            </div>
 
-        <div>
-            <NavLink to={'/address'} className="px-2 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300">Proceed to Address</NavLink>
-            <NavLink to={'/dashboard'} className="px-2 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300">Back to shopping</NavLink>
+            <div className='flex gap-x-10'>
+                <NavLink to={'/address'} className="px-2 py-2 border border-gray-400 text-black font-normal rounded-md">Proceed to Address</NavLink>
+                <NavLink to={'/dashboard'} className="px-2 py-2 border border-gray-400 text-black font-normal rounded-md">Back to shopping</NavLink>
+            </div>
         </div>
     </div>
   )
