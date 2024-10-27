ac2041c32a2798ab488388bb2ae926a79643c8cf
Omar Mousa <omar.s.mousa194@gmail.com>
2024-10-27 11:44:20 +0300

    edit width login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,8 +11,11 @@ export const Login: React.FC = () => {
     login();
   };
 
+  // relative h-screen w-screen bg-cover bg-center
+  // style={{ backgroundImage: `url('https://s3-alpha-sig.figma.com/img/c230/ffba/520fab60716f712257d7f6a7fc48a42f?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mbNGe5cnciSearQhKP2TR~WMvIdiSi38k2DKnj9az4oIk~mkYPlBew~n6b49iJlGa1MfnLR99ignFHZWU834sozpXh0ZES44LHTkQ~LaiPX617O9vsGdLOdBzv~HfyHC4i-gxBdZJ2-C8~dWWDlhfVZzmyYsfCpk3l9--rQ98~X9rgcTKOzt219SqTjuf61A81vMm8zHed3JRxZAWpgvz2cg1aRW59eJm4F35rTeWhNY~WCR3y8J7WNm3lUYI6JqRMw2~D2vx6ZCJ4LNuHSKcJSt0dJWnwlIJ1V7iGfrHlbC5Cia~DlDhNel5OLxptWbH0JlwdpO0trcmk4kZK0hog__')`}}
+
   return (
-    <div className='relative flex justify-center items-center h-screen w-screen bg-cover bg-center' style={{ backgroundImage: `url('https://s3-alpha-sig.figma.com/img/c230/ffba/520fab60716f712257d7f6a7fc48a42f?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mbNGe5cnciSearQhKP2TR~WMvIdiSi38k2DKnj9az4oIk~mkYPlBew~n6b49iJlGa1MfnLR99ignFHZWU834sozpXh0ZES44LHTkQ~LaiPX617O9vsGdLOdBzv~HfyHC4i-gxBdZJ2-C8~dWWDlhfVZzmyYsfCpk3l9--rQ98~X9rgcTKOzt219SqTjuf61A81vMm8zHed3JRxZAWpgvz2cg1aRW59eJm4F35rTeWhNY~WCR3y8J7WNm3lUYI6JqRMw2~D2vx6ZCJ4LNuHSKcJSt0dJWnwlIJ1V7iGfrHlbC5Cia~DlDhNel5OLxptWbH0JlwdpO0trcmk4kZK0hog__')` }}>
+    <div className='flex justify-center items-center'>
       <form onSubmit={handleLogin} className='flex flex-col justify-center items-center space-y-8 bg-green-50 rounded-lg py-2 px-5 shadow-black shadow-2xl'>
         <div>
           <h2 className='text-4xl my-5 font-bold'>Welcome back!</h2>
