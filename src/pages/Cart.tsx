5b093735fac842bbf6a56b327ce210dd8b67537b
Omar Mousa <omar.s.mousa194@gmail.com>
2024-10-28 10:30:21 +0300

    style on cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { store } from '../hooks/store'
 import { NavLink } from 'react-router-dom';
 import { useCalculateTotal } from '../hooks/useCalculateTotal';
@@ -16,45 +16,53 @@ export const Cart = () => {
         loadCart();
     }, [loadCart]);
 
+    const isEmpty = cart.length === 0;
+
 
   return (
     <div className=''>
         <h1 className='text-3xl font-semibold mb-6 text-center'>Shopping cart</h1>
         <div className='flex flex-col justify-center items-center rounded-lg shadow-2xl shadow-black m-4 p-5'>
-            <div className='space-y-4 w-full'>
-                {/* <div className='grid grid-cols-3 place-items-center mb-12'>
-                    <p className='font-bold text-2xl italic'>Product</p>
-                    <p className='font-bold text-2xl italic'>Price</p> 
-                    <p className='font-bold text-2xl italic'>Quantity</p> 
-                </div> */}
-                {cart.map((item) => (
-                    <>
-                    <div key={item.id} className='grid grid-cols-4 place-content-evenly place-items-center'>
-                        <img
-                            src={item.image}
-                            alt={item.title}
-                            className='w-14 h-14 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-44 lg:h-44 object-contain block rounded-md mb-4'
-                        />
-                        <p className='text-left'>{item.title}</p>
-                        <p className=''>${item.price ? item.price.toFixed(2) : 0.00}</p>
-                        <div className='flex flex-col-reverse justify-center items-center sm:flex-row sm:space-x-3'>
-                            <button onClick={() => decreaseQuantity(item.id)} className='flex justify-center items-center border border-gray-400 text-gray-400 px-4 rounded-md w-8 h-8'>-</button>
-                            <p>{item.quantity}</p>
-                            <button onClick={() => increaseQuantity(item.id)} className='flex justify-center items-center border border-gray-400 text-gray-400 px-4 rounded-md w-8 h-8'>+</button>
+            {isEmpty ? (
+                <p>Your cart is empty</p>
+            ) : (
+                <div className='space-y-4 w-full'>
+                    {/* <div className='grid grid-cols-3 place-items-center mb-12'>
+                        <p className='font-bold text-2xl italic'>Product</p>
+                        <p className='font-bold text-2xl italic'>Price</p> 
+                        <p className='font-bold text-2xl italic'>Quantity</p> 
+                    </div> */}
+                    {cart.map((item) => (
+                        <>
+                        <div key={item.id} className='grid grid-cols-4 place-content-evenly place-items-center'>
+                            <img
+                                src={item.image}
+                                alt={item.title}
+                                className='w-14 h-14 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-44 lg:h-44 object-contain block rounded-md mb-4'
+                            />
+                            <p className='text-left'>{item.title}</p>
+                            <p className=''>${item.price ? item.price.toFixed(2) : 0.00}</p>
+                            <div className='flex flex-col-reverse justify-center items-center sm:flex-row sm:space-x-3'>
+                                <button onClick={() => decreaseQuantity(item.id)} className='flex justify-center items-center border border-gray-400 text-gray-400 px-4 rounded-md w-8 h-8'>-</button>
+                                <p>{item.quantity}</p>
+                                <button onClick={() => increaseQuantity(item.id)} className='flex justify-center items-center border border-gray-400 text-gray-400 px-4 rounded-md w-8 h-8'>+</button>
+                            </div>
                         </div>
-                    </div>
-                    <hr />
-                    </>
-                ))}
-            </div>
+                        <hr />
+                        </>
+                    ))}
+                </div>
+            )}
             
-            <div className='flex justify-center items-center gap-x-5 my-6 p-2 rounded-lg shadow-lg text-xl font-semibold border border-gray-500 w-max'>
-                <span>Total:</span>
-                <span>${calTotal()}</span>
-            </div>
+            {isEmpty && (
+                <div className='flex justify-center items-center gap-x-5 my-6 p-2 rounded-lg shadow-lg text-xl font-semibold border border-gray-500 w-max'>
+                    <span>Total:</span>
+                    <span>${calTotal()}</span>
+                </div>
+            )}
 
-            <div className='flex gap-x-10 text-center'>
-                <NavLink to={'/address'} className="px-2 py-2 border border-gray-400 text-black font-normal rounded-md">Proceed to Address</NavLink>
+            <div className='flex flex-col gap-x-10 gap-y-2 sm:flex-row sm:items-center sm:justify-center text-center w-full'>
+                <NavLink to={'/address'} className={`px-2 py-2 border border-gray-400 text-black font-normal rounded-md ${isEmpty ? 'opacity-50 cursor-not-allowed' : 'text-black'}`}>Proceed to Address</NavLink>
                 <NavLink to={'/dashboard'} className="px-2 py-2 border border-gray-400 text-black font-normal rounded-md">Back to shopping</NavLink>
             </div>
         </div>
